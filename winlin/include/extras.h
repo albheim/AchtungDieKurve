baff1f2afe00efd2242c5329005faaa1daf36ad6
Adrian Roth <adrianroth10@gmail.com>
2016-01-22 20:30:26 +0100

    tog bort winlin

--- winlin/include/extras.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXTRAS_H
-#define EXTRAS_H
-
-#include<time.h>
-
-void delay(int ms);
-
-#endif
--- winlin/src/extras.c
+++
@@ -1,7 +0,0 @@
-#include"extras.h"
-
-void delay(int ms)
-{
-	clock_t end = clock() +	ms*CLOCKS_PER_SEC/1000;
-	while(end > clock());
-}
